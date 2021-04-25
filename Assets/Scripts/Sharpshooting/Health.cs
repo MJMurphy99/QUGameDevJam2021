e8134e2323320351179b43ace266bcf2bdd0dcfd
AraerTalsen <43824879+AraerTalsen@users.noreply.github.com>
2021-04-25 17:10:34 -0400

    Can die now

--- Assets/Scripts/Sharpshooting/Health.cs
+++ Assets/Scripts/Sharpshooting/Health.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class Health : MonoBehaviour
 {
@@ -25,7 +26,17 @@ public class Health : MonoBehaviour
         currentHealth -= dmg;
         healthBar.value = currentHealth / baseHealth;
         healthLabel.text = currentHealth + " / " + baseHealth;
-        float alpha = currentHealth / baseHealth;
+        float alpha = 1 - (currentHealth / baseHealth);
         redTinge.color = new Color(1, 0, 0, alpha);
+
+        if (currentHealth <= 0) Dead();
+    }
+
+    public void Dead()
+    {
+        for (int i = 1; i < GlobalControl.fish.Length; i++)
+            GlobalControl.fish[i] = 0;
+
+        SceneManager.LoadScene("Fishing");
     }
 }
